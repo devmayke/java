62a507bb113dd5686a4c76759d9683cb3adb31e2
Mayke Ken <devmayke@gmail.com>
2021-09-04 17:28:37 -0300

    Renomeado os nomes de váariáaveis

--- src/fundamentos/ConversorTemperatura.java
+++ src/fundamentos/ConversorTemperatura.java
@@ -3,11 +3,11 @@ package fundamentos;
 public class ConversorTemperatura {
 
   public static void main( String[] args){
-    double valorInicial = 5;
-    final double eq1 = 32;
-    final double eq2 = 5. / 9.;
-    double resultado = (valorInicial - eq1) * eq2;   
-    System.out.println(resultado);
+    double fahrenheit = 5;
+    final double diminui = 32;
+    final double multiplicador  = 5. / 9.;
+    double celsius = (fahrenheit - diminui) * multiplicador;   
+    System.out.println(celsius);
   }
   
 }
