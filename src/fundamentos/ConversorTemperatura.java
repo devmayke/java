05fc7d55666a4a53531c4e4b117bd4e42d8ce4df
Mayke Ken <devmayke@gmail.com>
2021-09-04 17:32:20 -0300

    Renmomeado nome de constante para maisculo por questãao de boa pratica

--- src/fundamentos/ConversorTemperatura.java
+++ src/fundamentos/ConversorTemperatura.java
@@ -4,9 +4,9 @@ public class ConversorTemperatura {
 
   public static void main( String[] args){
     double fahrenheit = 5;
-    final double diminui = 32;
-    final double multiplicador  = 5. / 9.;
-    double celsius = (fahrenheit - diminui) * multiplicador;   
+    final double DIMINUI  = 32;
+    final double MULTIPLICADOR  = 5. / 9.;
+    double celsius = (fahrenheit - DIMINUI) e MULTIPLICADOR;   
     System.out.println(celsius);
   }
   
