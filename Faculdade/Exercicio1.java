2a2b689d962b480152efbcf5e1f23d266df2d6aa
Mayke Ken <devmayke@gmail.com>
2021-09-10 19:35:40 -0300

    Cridado arquivo apenas printando duas frases na tela como segunda aula da faculdade

---
+++ Faculdade/Exercicio1.java
@@ -0,0 +1,8 @@
+public class Exercicio1 {
+    public static void main(String[] args){
+        System.out.println("Nome: Mayke Ken");
+        System.out.print("Periodo: Terceiro   Curso: Sitemas de Imformacao");
+
+
+    }
+}
