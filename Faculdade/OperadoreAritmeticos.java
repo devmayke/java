62aedc3c49c176aec8be59ffc9a32abb143b4633
Mayke Ken <devmayke@gmail.com>
2021-09-10 20:34:46 -0300

    Operadores aritiméticos

---
+++ Faculdade/OperadoreAritmeticos.java
@@ -0,0 +1,12 @@
+public class OperadoreAritmeticos {
+    public static void main(String[] args){
+        Integer numero = 10;
+        System.out.println(numero + 950);
+        System.out.println(numero - 89);
+        System.out.println(numero * 2);
+        System.out.println(numero / 3);
+        System.out.println(numero % 3);
+        System.out.println(10 % 3);
+
+    }
+}
