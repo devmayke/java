1bc4245ca049e4f95d4ab3d494795a88953011cf
Mayke Ken <devmayke@gmail.com>
2021-09-24 20:10:07 -0300

    Funções de tratamento de String

---
+++ Faculdade/Exercicio2.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Exercicio2 {
+    public static void main(String[] args){
+        String str= "Curso de Java";
+        System.out.println(str.equals("sdsad"));
+        System.out.println(str.equals("Curso de Java"));
+        System.out.println(str.charAt(2));
+        System.out.println(str.startsWith("Cu"));
+        System.out.println(str.endsWith("va"));
+        System.out.println(str.substring(9));
+        System.out.println(str.substring(3,11));
+        System.out.println(str.replace("de ","" ));
+
+    }
+
+
+
+}
